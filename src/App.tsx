57ea3a23f4533ee20eee34e46876476e745a52de
Himanshu8747 <himanshu8747@gmail.com>
2024-12-15 09:58:56 +0530

    Add Community Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Features from "./components/features/Features";
 import { SignInSignUp } from "./components/auth/SignInSignUp";
 import CodeEditor from "./components/code-editor/editor";
+import { CommunityPage } from "./components/community/ComunityPage";
+import { ProjectsPage } from "./components/project/ProjectsPage";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path="/features" element={<Features/>} />
           <Route path="/auth" element={<SignInSignUp/>} />
           <Route path="/editor" element={<CodeEditor/>} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/projects" element={<ProjectsPage/>} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
