7c9b0b6a6c38f010a632c0b2eb7a2619d6f5d017
Himanshu8747 <himanshu8747@gmail.com>
2024-12-18 16:58:47 +0530

    COmponentize the CollaborationHub Component into Community and Project

--- src/components/community/ComunityPage.tsx
+++ src/components/community/ComunityPage.tsx
@@ -1,5 +1,4 @@
 import { FeaturedDevelopers } from './FeaturedDevelopers';
-import { CollaborationHub } from './CollaborationHub';
 import { IdeaBoard } from './IdeaBoard';
 import { LiveChat } from './LiveChat';
 import { ideas } from '../../data/dummyData';
@@ -13,7 +12,6 @@ export function CommunityPage() {
       
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2 space-y-8">
-          <CollaborationHub />
           <IdeaBoard ideas={ideas} />
         </div>
         <div>
