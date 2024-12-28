6b934b81da1bc0abf5b436a248c220bc76b85a8c
Himanshu8747 <himanshu8747@gmail.com>
2024-12-28 13:33:21 +0530

    Add Protected Route for Chat Component

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,12 @@ import CodeEditor from "./components/code-editor/editor";
 import { CommunityPage } from "./components/community/ComunityPage";
 import { Projects } from "./components/projects/Projects";
 import { ProjectDetails } from "./components/projects/ProjectDetails";
-
+import EnhancedLiveChat from "./components/community/EnhancedLiveChat";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
 function App() {
+  const user = useSelector((state: RootState) => state.auth.user);
   return (
     <BrowserRouter>
       <MainLayout>
@@ -19,6 +23,14 @@ function App() {
           <Route path="/editor" element={<CodeEditor />} />
           <Route path="/community" element={<CommunityPage />} />
           <Route path="/projects" element={<Projects />} />
+          <Route 
+            path="/chatBoard" 
+            element={
+              <ProtectedRoute>
+                <EnhancedLiveChat currentUser={user!} />
+              </ProtectedRoute>
+            } 
+          />
           <Route path="/projects/:projectId" element={<ProjectDetails />} />
         </Routes>
       </MainLayout>
